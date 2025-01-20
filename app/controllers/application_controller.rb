61eded35a340ee1a1f04b75e2b1e8c35ab2520e4
chepyego <jchepyego@gmail.com>
2025-01-21 01:33:30 +0300

    add before action to allow unauthenticated user to acces landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  # before_action :authenticated?
+  # before_action :authenticate_user!, only: [ :new, :create, :edit ]
+  # allow_unauthenticated_access only: %i[ index show ]
+
+
+
+
+  include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
