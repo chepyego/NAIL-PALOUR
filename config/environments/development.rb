ac61b579dc7c97629ad62f9f1c5c528084570fc3
chepyego <jchepyego@gmail.com>
2025-04-05 02:12:46 +0300

    deleted tailind files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,11 @@ Rails.application.configure do
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
+  # tailwindcss loading
+  config.assets.debug = true
+
+
+
   # Do not eager load code on boot.
   config.eager_load = false
 
