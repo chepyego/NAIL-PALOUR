15ee6f16d6b7d8a2e819c06622fd5f3dae111753
chepyego <jchepyego@gmail.com>
2025-06-12 17:22:42 +0300

    remove ash_key column from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_02_013255) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_12_142110) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -87,6 +87,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_02_013255) do
     t.index ["user_id"], name: "index_manicurists_on_user_id"
   end
 
+  create_table "playing_with_neon", id: :serial, force: :cascade do |t|
+    t.text "name", null: false
+    t.float "value", limit: 24
+  end
+
   create_table "schedules", force: :cascade do |t|
     t.string "title"
     t.datetime "start"
@@ -119,7 +124,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_02_013255) do
     t.datetime "expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "key_hash"
     t.index ["key"], name: "index_solid_cache_entries_on_key", unique: true
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash"
   end
 
   create_table "users", force: :cascade do |t|
