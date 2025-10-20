dbf3844c155ea19ce543a0de9247558db5088180
chepyego <jchepyego@gmail.com>
2025-10-20 20:11:40 +0300

    add solid table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_01_132130) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_17_231328) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -119,14 +119,19 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_01_132130) do
   end
 
   create_table "solid_cache_entries", force: :cascade do |t|
-    t.string "key", null: false
-    t.text "value"
-    t.datetime "expires_at"
+    t.binary "key", null: false
+    t.binary "value", null: false
+    t.datetime "created_at", null: false
+    t.bigint "key_hash", null: false
+    t.integer "byte_size", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
+  end
+
+  create_table "solid_cache_tables", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "key_hash"
-    t.index ["key"], name: "index_solid_cache_entries_on_key", unique: true
-    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash"
   end
 
   create_table "users", force: :cascade do |t|
