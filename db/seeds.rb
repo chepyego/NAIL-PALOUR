df9ec6fc94f769d0250eeaf17a0e12111a774dbf
chepyego <jchepyego@gmail.com>
2025-10-20 20:44:50 +0300

    edit seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Create an administrator account if it doesn't already exist.
 User.find_or_create_by!(email_address: "jchepyego@gmail.com") do |user|
-    user.password = "jemimah21"
-    user.role = "admin"
-  end
+  # Add the required name field, if your User model validates its presence.
+  user.name = "Jemimah Chepyego"
+
+  user.password = "jemimah21"
+  # IMPORTANT: Password confirmation is required by most Rails authentication setups.
+  user.password_confirmation = "jemimah21"
+
+  user.role = "admin"
+end
